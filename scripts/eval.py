35694c00993482694b4c42e1c870850ad2ce47e3
Andras Palasti <andraspalasti2@gmail.com>
2025-01-15 22:02:19 +0100

    add vector search and update results

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,10 +1,12 @@
+import math
 from pathlib import Path
+from typing import Dict
 
 import ranx
-import orjson
 import pandas as pd
 from tqdm import tqdm
 from src.vector_db import VectorDB
+from sentence_transformers import SentenceTransformer
 
 
 ROOT_DIR = Path(__file__).parent.parent
@@ -12,28 +14,54 @@ CORPUS_PATH = ROOT_DIR / "data/msmarco/test_corpus.jsonl"
 CUTOFF = 100
 
 
+class BM25Search:
+    name: str = "BM25"
+
+    def __call__(self, db: VectorDB, query):
+        return db.bm25_search(query["text"], cutoff=CUTOFF)
+
+
+class VectorSearch:
+    name = "vector-search"
+
+    def __call__(self, db: VectorDB, query):
+        result = db.vector_search(query["embedding"], cutoff=CUTOFF)
+        result["_score"] = 1 - result["_distance"]
+        return result
+
+
 def file_len(fpath):
     with open(fpath, "r") as f:
         return sum(1 for _ in f)
 
 
-def read_jsonl(fpath, callback):
-    with open(fpath, "r") as f:
-        for line in f:
-            yield callback(orjson.loads(line))
+def calculate_embeddings(model: SentenceTransformer, batch: pd.DataFrame):
+    inputs = batch["text"].astype("str").to_list()
+    batch["embedding"] = list(model.encode(inputs, normalize_embeddings=True))
+    return batch
 
 
 def main():
+    print("Loading embedding model...")
+    model = SentenceTransformer(
+        "sentence-transformers/all-MiniLM-L6-v2",
+    )
+
     print("Loading database...")
-    db = VectorDB(ROOT_DIR / "data/msmarco/msmarco.db", 0)
+    db = VectorDB(
+        ROOT_DIR / "data/msmarco/msmarco.db",
+        model.get_sentence_embedding_dimension(),
+    )
 
     # If db is empty then load the corpus
     if 0 == db.count_rows():
-        num_lines = file_len(CORPUS_PATH)
-        record_generator = read_jsonl(
-            CORPUS_PATH, lambda row: [{"id": row["_id"], "text": row["text"]}]
+        batch_size = 512
+        num_batches = math.ceil(file_len(CORPUS_PATH) / batch_size)
+        batch_generator = (
+            calculate_embeddings(model, batch.rename(columns={"_id": "id"}))
+            for batch in pd.read_json(CORPUS_PATH, lines=True, chunksize=batch_size)
         )
-        db.add(tqdm(record_generator, desc="Loading corpus...", total=num_lines))
+        db.add(tqdm(batch_generator, desc="Loading corpus...", total=num_batches))
     else:
         print("Corpus already loaded!")
 
@@ -44,17 +72,25 @@ def main():
     queries = queries.set_index("_id", drop=False).rename(columns={"_id": "id"})
     queries = queries.loc[qd_pairs.index.unique()]
     queries["text"] = queries["text"].astype("str")
+    queries = calculate_embeddings(model, queries)
 
-    run = ranx.Run({
-        str(query_id): db.bm25_search(query, cutoff=CUTOFF)
-        .to_pandas()
-        .astype({"id": "str"})
-        .set_index("id")["_score"]
-        .to_dict()
-        for query_id, query in tqdm(
-            queries["text"].items(), desc="Running queries", total=len(queries)
-        )
-    })
+    search_methods = [BM25Search(), VectorSearch()]
+
+    runs = []
+    for method in search_methods:
+        with tqdm(
+            queries.index,
+            desc=f"Running queries with {method.name}",
+            leave=True,
+            unit="query",
+        ) as pbar:
+            run_dict = {
+                str(query_id): (df := method(db, queries.loc[query_id]))
+                .set_index(df.index.astype("str"))["_score"]
+                .to_dict()
+                for query_id in pbar
+            }
+        runs.append(ranx.Run(run_dict, method.name))
 
     qrels = ranx.Qrels(
         {
@@ -65,7 +101,9 @@ def main():
         }
     )
 
-    report = ranx.compare(qrels, runs=[run], metrics=["hits", "recall", "precision@5", "precision@10"])
+    report = ranx.compare(
+        qrels, runs, metrics=["hits", "recall", "precision", "precision@5", "precision@10"]
+    )
     print(report)
 
 
--- src/vector_db.py
+++ src/vector_db.py
@@ -1,7 +1,8 @@
 import lancedb
-import lance 
-import lancedb.exceptions
+import numpy as np
+import pandas as pd
 import pyarrow as pa
+
 
 TABLE_NAME = "embeddings"
 
@@ -13,19 +14,22 @@ class VectorDB:
         self.setup()
 
     def setup(self):
-        schema = pa.schema([
-            pa.field("id", pa.int64()),
-            pa.field("file_path", pa.string()),
-            pa.field("embedding", pa.list_(pa.float32(), list_size=self.embedding_dim)),
-            pa.field("text", pa.string()),
-        ])
+        schema = pa.schema(
+            [
+                pa.field("id", pa.int64()),
+                pa.field("file_path", pa.string()),
+                pa.field(
+                    "embedding", pa.list_(pa.float32(), list_size=self.embedding_dim)
+                ),
+                pa.field("text", pa.string()),
+            ]
+        )
 
         self.table = self.db.create_table(TABLE_NAME, schema=schema, exist_ok=True)
         try:
             self.table.create_fts_index(field_names="text", use_tantivy=False)
         except:
             pass
-
 
     def add(self, data):
         return self.table.add(data)
@@ -34,8 +38,22 @@ class VectorDB:
         return self.table.count_rows()
 
     def bm25_search(self, query: str, cutoff: int):
-        return (
+        result: pd.DataFrame = (
             self.table.search(query=query, query_type="fts", fts_columns="text")
             .limit(cutoff)
             .select(["id", "text"])
+            .to_pandas()
         )
+        result.set_index("id", inplace=True)
+        return result
+
+    def vector_search(self, vector: np.ndarray, cutoff: int):
+        result: pd.DataFrame = (
+            self.table.search(vector, vector_column_name="embedding")
+            .metric("dot")
+            .limit(cutoff)
+            .select(["id", "text"])
+            .to_pandas()
+        )
+        result.set_index("id", inplace=True)
+        return result
